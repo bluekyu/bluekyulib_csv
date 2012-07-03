6976b04a19fcb1f4a50e07bde1195c140c47f3c4
YoungUk Kim <bluekyu.dev@gmail.com>
2012-07-03 16:36:15 +0900

    Some function is added
    6 functions is added and need the test

--- src/csv.cpp
+++ src/csv.cpp
@@ -1,1 +1,55 @@
 #include "csv.h"
+
+/*** Start bluekyulib namespace **********************************************/
+namespace bluekyulib
+{
+
+/*** Start csv namespace *****************************************************/
+namespace csv
+{
+
+bool CSV::ReadFile(const std::string &fileName)
+{
+    return true;
+}
+
+bool CSV::WriteFile(const std::string &fileName)
+{
+    return true;
+}
+
+void CSV::InsertLine(const int &index, const row_t &line)
+{
+    table_t::iterator iter = table.begin();
+
+    for (int i = 0; i < index; i++)
+        iter++;
+
+    table.insert(iter, line);
+}
+
+void CSV::AppendLine(const row_t &line)
+{
+    table.push_back(line);
+}
+
+void CSV::PopLine(void)
+{
+    table.pop_back();
+}
+
+void CSV::RemoveLine(const int &index)
+{
+    table_t::iterator iter = table.begin();
+
+    for (int i = 0; i < index; i++)
+        iter++;
+    
+    table.erase(iter);
+}
+
+}
+/*** End csv namespace *******************************************************/
+
+}
+/*** End bluekyulib namespace ************************************************/
--- src/csv.h
+++ src/csv.h
@@ -1,4 +1,5 @@
-#include <iostream>
+#include <vector>
+#include <string>
 
 /*** Start bluekyulib namespace **********************************************/
 namespace bluekyulib
@@ -7,9 +8,22 @@ namespace bluekyulib
 /*** Start csv namespace *****************************************************/
 namespace csv
 {
+typedef std::string data_t;
+typedef std::vector<data_t> row_t;
+typedef std::vector<row_t> table_t;
 
 class CSV
 {
+public:
+    bool ReadFile(const std::string &fileName);
+    bool WriteFile(const std::string &fileName);
+    void InsertLine(const int &index, const row_t &line);
+    void AppendLine(const row_t &line);
+    void PopLine(void);
+    void RemoveLine(const int &index);
+
+private:
+    table_t table;
 };
 
 }
